8748f8ada2ffa6d51947f3e7174e7bba74fa0925
rico <1476262983@qq.com>
2024-01-30 03:27:45 +0800

    2024.1.30 优化分层逻辑

---
+++ src/main/java/com/rico/rbi/model/entity/User.java
@@ -0,0 +1,66 @@
+package com.rico.rbi.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName User
+ */
+@TableName(value ="User")
+@Data
+public class User implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 
+     */
+    private String userAccount;
+
+    /**
+     * 
+     */
+    private String userPassword;
+
+    /**
+     * 
+     */
+    private String userName;
+
+    /**
+     * 
+     */
+    private String userAvatar;
+
+    /**
+     * 
+     */
+    private String userRole;
+
+    /**
+     * 
+     */
+    private Date createTime;
+
+    /**
+     * 
+     */
+    private Date updateTime;
+
+    /**
+     * 
+     */
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
