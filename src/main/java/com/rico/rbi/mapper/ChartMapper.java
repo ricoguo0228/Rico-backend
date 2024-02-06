44c25a45f7bd1da9209a250d403e6cdbcbdd442e
1476262983 <1476262983@qq.com>
2024-02-07 01:17:58 +0800

    1.0 Chart 内容重置

---
+++ src/main/java/com/rico/rbi/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.rico.rbi.mapper;
+
+import com.rico.rbi.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 14762
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2024-02-07 00:11:39
+* @Entity com.rico.rbi.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
